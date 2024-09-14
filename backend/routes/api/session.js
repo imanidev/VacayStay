b8d0042a2e87c4965976648d71f267041744d059
Imani Roberts <imaniroberts.dev@gmail.com>
2024-09-14 14:57:12 -0400

    added restore session user

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -7,9 +7,6 @@ const { User } = require("../../db/models");
 
 const router = express.Router();
 
-// backend/routes/api/session.js
-// ...
-
 // Log in
 router.post("/", async (req, res, next) => {
   const { credential, password } = req.body;
@@ -49,6 +46,24 @@ router.delete(
   (_req, res) => {
     res.clearCookie('token');
     return res.json({ message: 'success' });
+  }
+);
+
+// Restore session user
+router.get(
+  '/',
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+      return res.json({
+        user: safeUser
+      });
+    } else return res.json({ user: null });
   }
 );
 
