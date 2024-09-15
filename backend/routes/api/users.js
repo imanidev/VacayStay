8e073f345e7fe68fc6a1bfe459a832fcada7bcaf
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-09-15 16:37:06 -0400

    add validation imports

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -13,17 +13,19 @@ const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 
 const validateSignup = [
-  check("email").isEmail().withMessage("Please provide a valid email."),
+  check("email")
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage("Please provide a valid email."),
   check("username")
     .exists({ checkFalsy: true })
-    .withMessage("Please provide a username."),
-  check("username").not().isEmail().withMessage("Username cannot be an email."),
-  check("username")
     .isLength({ min: 4 })
     .withMessage("Please provide a username with at least 4 characters."),
-  check("username")
-    .isLength({ max: 30 })
-    .withMessage("Please provide a username with at most 30 characters."),
+  check("username").not().isEmail().withMessage("Username cannot be an email."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage("Password must be 6 characters or more."),
   handleValidationErrors,
 ];
 
--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,3 +1,4 @@
+// backend/utils/validation.js
 const { validationResult } = require("express-validator");
 
 // middleware for formatting errors from express-validator middleware
@@ -6,7 +7,10 @@ const handleValidationErrors = (req, _res, next) => {
   const validationErrors = validationResult(req);
 
   if (!validationErrors.isEmpty()) {
-    const errors = validationErrors.array().map((error) => `${error.msg}`);
+    const errors = {};
+    validationErrors
+      .array()
+      .forEach((error) => (errors[error.path] = error.msg));
 
     const err = Error("Bad request.");
     err.errors = errors;
