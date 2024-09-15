4b03dca86464d92d6b5e3a86f62217c3b3114a22
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-09-14 20:34:38 -0400

    add validate login middleware

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -26,6 +26,17 @@ const validateSignup = [
   handleValidationErrors,
 ];
 
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+
 // Sign up route
 router.post("/", validateSignup, async (req, res, next) => {
   try {
