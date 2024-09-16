99768a9413bec34f2020b91422b9835ea5235f70
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-09-16 11:20:57 -0400

    fix import sequelize op

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { requireAuth } = require("../../utils/auth");
 const { handleValidationErrors } = require("../../utils/validation");
 const { check } = require("express-validator");
+const { Op } = require("sequelize");
 
 const validateBooking = [
   // Check if startDate exists
